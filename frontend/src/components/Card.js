a9dd337183bfb6f9fcbd12d025bf33a6f53dca6d
emlai1263 <emlai1263@gmail.com>
2024-02-22 18:31:25 -0800

    card components updated

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,17 +1,49 @@
+import {motion} from 'framer-motion';
+import {useState} from 'react';
 import React from 'react';
 import './Card.css'
+import triangle from './triangle.png';
 
-function Card({title, body}){
-    return (
-        <div className='card-container'>
-            <div className='card-title'>
-                {title}
-            </div>
-            <div className='card-body'>
-                <p>{body}</p>
-            </div>
+function Card({title, dueDate, category, timeEst, body}){
+    const [isOpen, setIsOpen] = useState(false);
+
+    return(
+        <div className="App">
+            <motion.div 
+            transition={{layout : {duration : 1, type : "spring"}}}
+            layout onClick={() => setIsOpen(!isOpen)}
+            className="card">
+                <div className='card-header'>
+                    <motion.img 
+                        src={triangle} alt="triangle" 
+                        layout="position" 
+                        animate={{rotate: isOpen ? 180 : 0}} 
+                        className="triangle"
+                        transition={{layout : {duration : 2, type : "spring"}}}>
+                    </motion.img>
+                    <motion.h2 layout="position" className="title">{title}</motion.h2>
+                    <motion.h2 layout="position" className="dueDate">{dueDate}</motion.h2>
+                </div>
+                {isOpen && (
+                    <motion.div 
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    transition={{duration: 1}}
+                    className='card-body'>
+                        <div
+                        className='extra-header'>
+                            <h2 className="category">{category}</h2>
+                            <h2 className="timeEst">{timeEst} hours</h2>
+                        </div>
+                        <motion.div
+                        className='body'>
+                            <p>{body}</p>
+                        </motion.div>
+                    </motion.div>
+                )}
+            </motion.div>
         </div>
-        )
+    );
 }
 
 export default Card
