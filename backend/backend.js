962ae2bd0c9e98da9675afe13d94d3768c100df5
emlai1263 <emlai1263@gmail.com>
2024-03-04 17:17:51 -0800

    emilys backend (task backend)

--- backend/backend.js
+++ backend/backend.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
-const userServices = require("./user_services");
+const userServices = require("./userServices");
+const taskServices = require("./taskServices");
 
 const app = express();
 const port = 8000;
@@ -13,39 +14,57 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// get users
 app.get("/users", async (req, res) => {
   const name = req.query["name"];
   const username = req.query["username"]; 
   try {
     const result = await userServices.getUsers(name);
-    res.send({ users_list: result });
+    res.send({ users: result });
   } catch (error) {
     console.log(error);
     res.status(500).send("An error ocurred in the server.");
   }
 });
 
+// get users by id
 app.get("/users/:id", async (req, res) => {
   const id = req.params["id"];
   const result = await userServices.findUserById(id);
   if (result === undefined || result === null)
     res.status(404).send("Resource not found.");
   else {
-    res.send({ users_list: result });
+    res.send({ users: result });
   }
 });
 
 // get tasks from a user
-app.get("/users/:user/tasks", async (req, res) => {
-  const user = req.params["user"];
-  const result = await userServices.findUserByUsername(user);
+app.get("/tasks/:id", async (req, res) => {
+  const userId = req.params["id"];
+  const result = await taskServices.getTasks(userId);
   if (result === undefined || result === null)
-    res.status(404).send("Resource not found.");
+    res.status(404).send("Resource not found :(");
   else {
-    res.send({ users_list: result });
+    res.send({ users: result });
   }
 });
 
+// delete a task from user
+app.delete("/tasks/:id", async (req, res) => {
+  const id = req.params["id"];
+  if (taskServices.deleteTask(user, id)) res.status(204).end();
+  else res.status(404).send("Resource not found.");
+});
+
+// task a task
+app.post("/tasks", async (req, res) => {
+  const task = req.body;
+  const savedTask = await taskServices.addTask(task);
+  if (savedTask) res.status(201).send(savedTask);
+  else res.status(500).end();
+});
+
+// add users
 app.post("/users", async (req, res) => {
   const user = req.body;
   const savedUser = await userServices.addUser(user);
