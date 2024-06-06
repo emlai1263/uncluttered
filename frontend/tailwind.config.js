c7d34eb5eb1bf656c5164e2a61c1d445f02f05ee
angelakim <akim173@calpoly.edu>
2024-06-06 13:44:00 -0700

    Added red to color theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -15,6 +15,18 @@ module.exports = {
       "moss-dark": "#7D876D",
       white: "#fff",
       black: "#000",
+      red: {
+        50: "#fef2f2",
+        100: "#fee2e2",
+        200: "#fecaca",
+        300: "#fca5a5",
+        400: "#f87171",
+        500: "#ef4444",
+        600: "#dc2626",
+        700: "#b91c1c",
+        800: "#991b1b",
+        900: "#7f1d1d",
+      },
       gray: {
         50: "#f9fafb",
         100: "#f3f4f6",
@@ -37,7 +49,7 @@ module.exports = {
   plugins: [],
   variants: {
     extend: {
-      opacity: ['responsive', 'hover', 'focus', 'group-hover'],
+      opacity: ["responsive", "hover", "focus", "group-hover"],
     },
   },
 };
